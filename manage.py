c5fbcecfd05c931cbbbceda430adffd79f103bb4
myeonghan-nim <ataraxia9408@gmail.com>
2021-07-23 12:24:19 +0900

    Change: project name

--- manage.py
+++ manage.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python
-"""Django's command-line utility for administrative tasks."""
+'''Django's command-line utility for administrative tasks.'''
 import os
 import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_intro.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'introduction.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
